557adfe930f1664bfc886b91c8f5bd5e3c506c4a
Aymeric <aymeric.clement.pro@gmail.com>
2026-01-23 10:14:35 +0100

    Modification de la table USER pour ajouter des champs supplémentaires.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,18 +3,34 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_USERNAME', fields: ['username'])]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
+    #[ORM\Column(length: 180)]
+    private ?string $username = null;
+
+    /**
+     * @var list<string> The user roles
+     */
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
 
     #[ORM\Column(length: 255)]
     private ?string $first_name = null;
@@ -22,42 +38,97 @@ class User
     #[ORM\Column(length: 255)]
     private ?string $last_name = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $email = null;
-
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
 
-    #[ORM\Column(nullable: true)]
+    #[ORM\Column]
     private ?\DateTimeImmutable $updated_at = null;
 
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $deleted_at = null;
 
-    #[ORM\ManyToOne(inversedBy: 'users')]
-    private ?Classe $classe = null;
-
-    /**
-     * @var Collection<int, Grade>
-     */
-    #[ORM\OneToMany(targetEntity: Grade::class, mappedBy: 'student', orphanRemoval: true)]
-    private Collection $grades;
-
-    /**
-     * @var Collection<int, Grade>
-     */
-    #[ORM\OneToMany(targetEntity: Grade::class, mappedBy: 'teacher', orphanRemoval: true)]
-    private Collection $givenGrades;
-
-    public function __construct()
-    {
-        $this->grades = new ArrayCollection();
-        $this->givenGrades = new ArrayCollection();
-    }
+    #[ORM\Column(nullable: true)]
+    private ?int $class_id = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param list<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
+     */
+    public function __serialize(): array
+    {
+        $data = (array) $this;
+        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
+
+        return $data;
+    }
+
+    #[\Deprecated]
+    public function eraseCredentials(): void
+    {
+        // @deprecated, to be removed when upgrading to Symfony 8
     }
 
     public function getFirstName(): ?string
@@ -84,18 +155,6 @@ class User
         return $this;
     }
 
-    public function getEmail(): ?string
-    {
-        return $this->email;
-    }
-
-    public function setEmail(string $email): static
-    {
-        $this->email = $email;
-
-        return $this;
-    }
-
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->created_at;
@@ -113,7 +172,7 @@ class User
         return $this->updated_at;
     }
 
-    public function setUpdatedAt(?\DateTimeImmutable $updated_at): static
+    public function setUpdatedAt(\DateTimeImmutable $updated_at): static
     {
         $this->updated_at = $updated_at;
 
@@ -132,74 +191,14 @@ class User
         return $this;
     }
 
-    public function getClasse(): ?Classe
+    public function getClassId(): ?int
     {
-        return $this->classe;
+        return $this->class_id;
     }
 
-    public function setClasse(?Classe $classe): static
+    public function setClassId(?int $class_id): static
     {
-        $this->classe = $classe;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Grade>
-     */
-    public function getGrades(): Collection
-    {
-        return $this->grades;
-    }
-
-    public function addGrade(Grade $grade): static
-    {
-        if (!$this->grades->contains($grade)) {
-            $this->grades->add($grade);
-            $grade->setStudent($this);
-        }
-
-        return $this;
-    }
-
-    public function removeGrade(Grade $grade): static
-    {
-        if ($this->grades->removeElement($grade)) {
-            // set the owning side to null (unless already changed)
-            if ($grade->getStudent() === $this) {
-                $grade->setStudent(null);
-            }
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Grade>
-     */
-    public function getGivenGrades(): Collection
-    {
-        return $this->givenGrades;
-    }
-
-    public function addGivenGrade(Grade $givenGrade): static
-    {
-        if (!$this->givenGrades->contains($givenGrade)) {
-            $this->givenGrades->add($givenGrade);
-            $givenGrade->setTeacher($this);
-        }
-
-        return $this;
-    }
-
-    public function removeGivenGrade(Grade $givenGrade): static
-    {
-        if ($this->givenGrades->removeElement($givenGrade)) {
-            // set the owning side to null (unless already changed)
-            if ($givenGrade->getTeacher() === $this) {
-                $givenGrade->setTeacher(null);
-            }
-        }
+        $this->class_id = $class_id;
 
         return $this;
     }
