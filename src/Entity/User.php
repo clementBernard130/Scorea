2de55ae4db06164dccec59bf2e8fcb551b041747
Aymeric <aymeric.clement.pro@gmail.com>
2026-01-23 10:20:02 +0100

    Fix in User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -202,16 +202,4 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
 }
