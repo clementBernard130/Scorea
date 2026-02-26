aeb16d0c384b746f6d7e1770682d23f16f9c5249
esoriabonet <enzo.soriabonet.pro@gmail.com>
2026-02-26 11:16:02 +0100

    Ajout des champs pour créer une section

--- src/Controller/Admin/SectionsCrudController.php
+++ src/Controller/Admin/SectionsCrudController.php
@@ -3,9 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Sections;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class SectionsCrudController extends AbstractCrudController
@@ -15,14 +17,36 @@ class SectionsCrudController extends AbstractCrudController
         return Sections::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Section')
+            ->setEntityLabelInPlural('Sections')
+            ->setDefaultSort(['start_date' => 'DESC']);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->onlyOnIndex(),
+
+            TextField::new('name', 'Nom'),
+
+            DateField::new('start_date', 'Date de début'),
+            DateField::new('end_date', 'Date de fin'),
+
+            AssociationField::new('training', 'Formation')
+                ->setRequired(true),
+
+            AssociationField::new('users', 'Utilisateurs')
+                ->setTemplatePath('admin/field/users_badges.html.twig')
+                ->onlyOnIndex(),
+                
+            AssociationField::new('users', 'Utilisateurs')
+                ->autocomplete()
+                ->setFormTypeOption('by_reference', false)
+                ->onlyOnForms(),
         ];
     }
-    */
 }
