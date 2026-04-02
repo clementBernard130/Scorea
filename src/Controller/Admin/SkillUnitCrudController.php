27df2322c7bbb0ce680941940d178cc9e7ec5677
clementBernard <clement.bernard.pro130@gmail.com>
2026-04-02 11:00:01 +0200

    Ajout de la liasion bc et formation (gestion des urls)

--- src/Controller/Admin/SkillUnitCrudController.php
+++ src/Controller/Admin/SkillUnitCrudController.php
@@ -2,29 +2,103 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Trainings;
 use App\Form\SkillType;
 use App\Entity\SkillsUnit;
+use App\Repository\TrainingsRepository;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class SkillUnitCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private TrainingsRepository $trainingsRepository,
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return SkillsUnit::class;
     }
 
-    
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->update(Crud::PAGE_INDEX, Action::NEW, fn (Action $action) => $action
+                ->linkToUrl(fn (): string => $this->generateContextualNewUrl())
+            )
+            ->update(Crud::PAGE_INDEX, Action::EDIT, fn (Action $action) => $action
+                ->linkToUrl(fn (SkillsUnit $skillUnit): string => $this->generateContextualUrl(Action::EDIT, $skillUnit))
+            )
+            ->update(Crud::PAGE_DETAIL, Action::EDIT, fn (Action $action) => $action
+                ->linkToUrl(fn (SkillsUnit $skillUnit): string => $this->generateContextualUrl(Action::EDIT, $skillUnit))
+            )
+            ->update(Crud::PAGE_DETAIL, Action::INDEX, fn (Action $action) => $action
+                ->linkToUrl(fn (SkillsUnit $skillUnit): string => $this->generateContextualIndexUrl($skillUnit))
+            );
+    }
+
+    public function createEntity(string $entityFqcn): SkillsUnit
+    {
+        $skillUnit = new SkillsUnit();
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId');
+
+        if ($trainingId !== null) {
+            $training = $this->trainingsRepository->find($trainingId);
+
+            if ($training instanceof Trainings) {
+                $skillUnit->setTrainings($training);
+            }
+        }
+
+        return $skillUnit;
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $queryBuilder = parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters);
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId');
+
+        if ($trainingId !== null) {
+            $queryBuilder
+                ->andWhere('entity.trainings = :trainingId')
+                ->setParameter('trainingId', $trainingId);
+        }
+
+        return $queryBuilder;
+    }
+
     public function configureFields(string $pageName): iterable
     {
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId');
+
+        $trainingField = AssociationField::new('trainings', 'Formation')
+            ->autocomplete()
+            ->setRequired(false);
+
+        if ($trainingId !== null && Crud::PAGE_NEW === $pageName) {
+            $trainingField->setHelp('Formation préremplie depuis la formation d’origine.');
+        }
+
         return [
             IdField::new('id')->hideOnForm(),
             TextField::new('name', 'Nom du Bloc'),
+            $trainingField,
             TextEditorField::new('description', 'Description'),
             CollectionField::new('skills', 'Compétences')
                 ->setEntryType(SkillType::class) // Votre formulaire Symfony
@@ -57,10 +131,96 @@ class SkillUnitCrudController extends AbstractCrudController
 
     public function configureCrud(Crud $crud): Crud
     {
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId');
+
         return $crud
-            ->setPageTitle('index', 'Blocs de compétences')
-            ->setPageTitle('new', 'Créer un bloc')
+            ->setPageTitle('index', $trainingId !== null ? 'Blocs de compétences de la formation' : 'Blocs de compétences')
+            ->setPageTitle('new', $trainingId !== null ? 'Créer un bloc pour la formation' : 'Créer un bloc')
             ->setPageTitle('edit', 'Modifier le bloc');
     }
 
+    protected function getRedirectResponseAfterSave(AdminContext $context, string $action): RedirectResponse
+    {
+        $trainingId = $context->getRequest()->query->get('trainingId');
+
+        if ($trainingId !== null) {
+            $submitButtonName = $context->getRequest()->request->all()['ea']['newForm']['btn'] ?? null;
+
+            $url = match ($submitButtonName) {
+                Action::SAVE_AND_CONTINUE => $this->adminUrlGenerator
+                    ->unsetAll()
+                    ->setController(self::class)
+                    ->setAction(Action::EDIT)
+                    ->setEntityId($context->getEntity()->getPrimaryKeyValue())
+                    ->set('trainingId', $trainingId)
+                    ->generateUrl(),
+                Action::SAVE_AND_ADD_ANOTHER => $this->adminUrlGenerator
+                    ->unsetAll()
+                    ->setController(self::class)
+                    ->setAction(Action::NEW)
+                    ->set('trainingId', $trainingId)
+                    ->generateUrl(),
+                default => $this->adminUrlGenerator
+                    ->unsetAll()
+                    ->setController(self::class)
+                    ->setAction(Action::INDEX)
+                    ->set('trainingId', $trainingId)
+                    ->generateUrl(),
+            };
+
+            return $this->redirect($url);
+        }
+
+        return parent::getRedirectResponseAfterSave($context, $action);
+    }
+
+    private function generateContextualUrl(string $action, SkillsUnit $skillUnit): string
+    {
+        $url = $this->adminUrlGenerator
+            ->unsetAll()
+            ->setController(self::class)
+            ->setAction($action)
+            ->setEntityId($skillUnit->getId());
+
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId');
+
+        if ($trainingId !== null) {
+            $url->set('trainingId', $trainingId);
+        }
+
+        return $url->generateUrl();
+    }
+
+    private function generateContextualNewUrl(): string
+    {
+        $url = $this->adminUrlGenerator
+            ->unsetAll()
+            ->setController(self::class)
+            ->setAction(Action::NEW);
+
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId');
+
+        if ($trainingId !== null) {
+            $url->set('trainingId', $trainingId);
+        }
+
+        return $url->generateUrl();
+    }
+
+    private function generateContextualIndexUrl(SkillsUnit $skillUnit): string
+    {
+        $url = $this->adminUrlGenerator
+            ->unsetAll()
+            ->setController(self::class)
+            ->setAction(Action::INDEX);
+
+        $trainingId = $this->getContext()?->getRequest()->query->get('trainingId')
+            ?? $skillUnit->getTrainings()?->getId();
+
+        if ($trainingId !== null) {
+            $url->set('trainingId', $trainingId);
+        }
+
+        return $url->generateUrl();
+    }
 }
--- src/Controller/Admin/TrainingCrudController.php
+++ src/Controller/Admin/TrainingCrudController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Trainings;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
@@ -13,6 +16,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class TrainingCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private AdminUrlGenerator $adminUrlGenerator
+    ) {}
+
     public static function getEntityFqcn(): string
     {
         return Trainings::class;
@@ -27,14 +34,33 @@ class TrainingCrudController extends AbstractCrudController
             ->setSearchFields(['name', 'description']);
     }
 
+    public function configureActions(Actions $actions): Actions
+    {
+        $skillUnitsAction = Action::new('skillUnits', 'Blocs de competences')
+            ->setIcon('fa fa-cubes')
+            ->linkToUrl(function (?Trainings $training): string {
+                if (!$training) return '';
+                
+                $action = $training->getSkillUnits()->isEmpty() ? Action::NEW : Action::INDEX;
+                
+                return $this->adminUrlGenerator->unsetAll()
+                    ->setController(SkillUnitCrudController::class)
+                    ->setAction($action)->set('trainingId', $training->getId())->generateUrl();
+            });
+
+        return $actions->add(Crud::PAGE_INDEX, $skillUnitsAction)
+            ->add(Crud::PAGE_DETAIL, $skillUnitsAction)
+            ->add(Crud::PAGE_NEW, $skillUnitsAction)
+            ->add(Crud::PAGE_EDIT, $skillUnitsAction);
+    }
+
     public function configureFields(string $pageName): iterable
     {
         yield IdField::new('id')
             ->hideOnForm();
 
         yield TextField::new('name', 'Nom')
-            ->setRequired(true)
-            ->setHelp('Nom affiché dans l’administration et dans les listes.');
+            ->setRequired(true);
 
         yield TextEditorField::new('description', 'Description')
             ->hideOnIndex()
@@ -45,10 +71,24 @@ class TrainingCrudController extends AbstractCrudController
             ->setSortable(false)
             ->formatValue(static fn ($value, Trainings $training): string => (string) $training->getSections()->count());
 
+        yield IntegerField::new('id', 'Nombre de blocs')
+            ->onlyOnIndex()
+            ->setSortable(false)
+            ->formatValue(static fn ($value, Trainings $training): string => (string) $training->getSkillUnits()->count());
+
         yield AssociationField::new('sections', 'Sections')
             ->onlyOnDetail();
 
         yield AssociationField::new('sections', 'Sections')
+            ->autocomplete()
+            ->setFormTypeOption('by_reference', false)
+            ->setRequired(false)
+            ->onlyOnForms();
+
+        yield AssociationField::new('skillUnits', 'Blocs de competences')
+            ->onlyOnDetail();
+
+        yield AssociationField::new('skillUnits', 'Blocs de competences')
             ->autocomplete()
             ->setFormTypeOption('by_reference', false)
             ->setRequired(false)
