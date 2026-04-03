7e4cd060e99cd733cecad5cedfb7fbda5316df73
clementBernard <clement.bernard.pro130@gmail.com>
2026-04-03 09:58:55 +0200

    redirige le role student dans l'espace eleve

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Users;
+use App\Service\StudentSkillsPageBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +11,19 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(StudentSkillsPageBuilder $studentSkillsPageBuilder): Response
     {
+        $user = $this->getUser();
+
+        if ($this->isGranted('ROLE_STUDENT') && $user instanceof Users) {
+            return $this->render('student/skills/index.html.twig', [
+                'page' => $studentSkillsPageBuilder->build($user),
+                'user' => $user,
+            ]);
+        }
+
         return $this->render('home/index.html.twig', [
-            'user' => $this->getUser(),
+            'user' => $user,
         ]);
     }
 }
