4eb6c026f144ae277466ad09aa76eb47cf1681bf
aclement0 <aymeric.clement.pro@gmail.com>
2026-04-27 10:32:29 +0200

    Ajout des tests sur la page ADMIN

--- src/Controller/Admin/TestsCrudController.php
+++ src/Controller/Admin/TestsCrudController.php
@@ -5,6 +5,10 @@ namespace App\Controller\Admin;
 use App\Entity\Tests;
 use App\Entity\Users;
 use App\Repository\UsersRepository;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -22,9 +26,54 @@ class TestsCrudController extends AbstractCrudController
         return Tests::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->showEntityActionsInlined()
+            ->setEntityLabelInSingular('Test')
+            ->setEntityLabelInPlural('Tests')
+            ->setPageTitle('index', 'Liste des tests')
+            ->setPageTitle('new', 'Créer un test')
+            ->setPageTitle('edit', 'Modifier le test')
+            ->setPageTitle('detail', 'Détails du test')
+            ->setDefaultSort(['testDate' => 'DESC'])
+            ->setSearchFields(['comment'])
+            ->overrideTemplates([
+                'crud/new' => 'admin/tests/test_new.html.twig',
+                'crud/edit' => 'admin/tests/test_edit.html.twig',
+                'crud/detail' => 'admin/tests/test_detail.html.twig',
+            ]);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('subject')
+            ->add('section')
+            ->add('teacher')
+            ->add('testDate');
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->remove(Crud::PAGE_NEW, Action::SAVE_AND_ADD_ANOTHER)
+            ->remove(Crud::PAGE_EDIT, Action::SAVE_AND_CONTINUE)
+            ->remove(Crud::PAGE_INDEX, Action::EDIT)
+            ->remove(Crud::PAGE_INDEX, Action::DELETE)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, static fn (Action $action): Action => $action
+                ->setIcon('fa fa-eye')
+                ->setLabel(false)
+                ->setHtmlAttributes(['title' => 'Consulter'])
+            );
+    }
+
     public function configureFields(string $pageName): iterable
     {
         return [
+            IdField::new('id')
+                ->onlyOnIndex(),
             AssociationField::new('subject', 'Matière')
                 ->setFormTypeOption('choice_label', 'name'),
             AssociationField::new('section', 'Section')
